cb66393a99684ca9af2b073574261e9d008ca178
Adonis <adonis.yu@cvsu.edu.ph>
2024-11-04 17:55:45 +0800

    - added functionality for uploading images using supabase - used this tutorial to know more about supabase storage. https://www.youtube.com/watch?v=HvOvdD2nX1k - I also added character counter

--- components/PostForm.jsx
+++ components/PostForm.jsx
@@ -1,13 +1,21 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { IoImageOutline } from "react-icons/io5";
 import Avatar from "./Avatar";
 import PostInput from "./PostInput";
 import { useSession } from "next-auth/react";
+import { supabase } from "@/services/supabase";
+import { v7 as uuidv7 } from "uuid";
+import { MAX_CHARS } from "@/lib/constants";
+import { FaSpinner } from "react-icons/fa6";
 
 const PostForm = () => {
   const [text, setText] = useState("");
+  const [currentImage, setCurrentImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState("");
+  const imagePickerRef = useRef(null);
+  const [isImageUploading, setIsImageUploading] = useState(false);
 
   const { data: session } = useSession();
 
@@ -15,24 +23,84 @@ const PostForm = () => {
     setText(evt.target.value);
   };
 
+  const handleImageUpload = (evt) => {
+    const file = evt.target.files[0];
+
+    if (file.size > 5 * 1024 * 1024) return;
+
+    if (file.type.split("/")[0] !== "image") {
+      alert("invalid file type");
+      return;
+    }
+
+    if (file) {
+      setCurrentImage(file);
+      // - convert the file into a local image, and set it as a url for the Image component
+      setImageUrl(URL.createObjectURL(file));
+
+      evt.target.value = "";
+    }
+  };
+
+  const handlePost = async () => {
+    const userId = session.user.userId;
+    setIsImageUploading(true);
+
+    try {
+      await supabase.storage
+        .from("images")
+        .upload(`${userId}/${uuidv7()}${currentImage.name}`, currentImage);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsImageUploading(false);
+      setText("");
+      setCurrentImage(null);
+      setImageUrl(null);
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setCurrentImage(null);
+    setImageUrl(null);
+  };
+
   return (
-    <div className="flex h-44 border-y-2 border-y-neutral-100">
+    <div className="flex h-auto border-y-2 border-y-neutral-100">
       {session && (
         <>
           <Avatar />
           <div className="flex h-full w-full flex-col">
-            <PostInput text={text} onChangeText={handleChangeText} />
+            <PostInput
+              onRemoveImage={handleRemoveImage}
+              text={text}
+              onChangeText={handleChangeText}
+              imageUrl={imageUrl}
+            />
 
             <div className="flex items-center justify-between p-2">
-              <IoImageOutline className="h-10 w-10 cursor-pointer rounded-full p-2 text-blue-500 transition-all hover:bg-neutral-200 sm:h-12 sm:w-12" />
+              <IoImageOutline
+                onClick={() => imagePickerRef.current.click()}
+                className="h-8 w-8 cursor-pointer rounded-full p-2 text-blue-500 transition-all hover:bg-neutral-200 sm:h-12 sm:w-12"
+              />
+              <input
+                type="file"
+                name=""
+                id=""
+                accept="image/*"
+                ref={imagePickerRef}
+                onChange={handleImageUpload}
+                hidden
+              />
+
               <button
-                onClick={() => {
-                  console.log(text);
-                }}
-                disabled={!text.length}
-                className={`tracking hover:bg-blue-600" rounded-full bg-blue-500 px-6 py-2 text-sm font-semibold uppercase text-blue-100 shadow-lg transition-all disabled:cursor-not-allowed disabled:opacity-80 sm:px-8`}
+                onClick={handlePost}
+                disabled={
+                  !text.length || text.length === MAX_CHARS || isImageUploading
+                }
+                className={`hover:bg-blue-600" rounded-full bg-blue-500 px-6 py-2 text-sm font-semibold uppercase tracking-wide text-white shadow-lg transition-all disabled:cursor-not-allowed disabled:opacity-80 disabled:brightness-95 sm:px-8`}
               >
-                Post
+                {isImageUploading ? "Loading.." : "post"}
               </button>
             </div>
           </div>
--- components/PostInput.jsx
+++ components/PostInput.jsx
@@ -1,16 +1,45 @@
 "use client";
 
-const PostInput = ({ onChangeText, text }) => {
+import { MAX_CHARS } from "@/lib/constants";
+import Image from "next/image";
+import { HiMiniXCircle } from "react-icons/hi2";
+
+const PostInput = ({ onChangeText, text, imageUrl, onRemoveImage }) => {
   return (
-    <textarea
-      value={text}
-      onChange={onChangeText}
-      className="text-md h-3/4 resize-none border-b-2 border-b-neutral-100 p-3 outline-none placeholder:text-lg placeholder:tracking-wide placeholder:text-neutral-600"
-      name=""
-      id=""
-      placeholder="What's happening?"
-      spellCheck={false}
-    />
+    <div className="flex flex-col gap-2">
+      <textarea
+        value={text}
+        onChange={onChangeText}
+        className="text-md h-24 resize-none pt-4 outline-none placeholder:text-lg placeholder:tracking-wide placeholder:text-neutral-600"
+        name=""
+        id=""
+        placeholder="What's happening?"
+        spellCheck={false}
+        minLength={0}
+        maxLength={MAX_CHARS}
+      />
+
+      {imageUrl && (
+        <div className="relative">
+          <Image
+            src={imageUrl}
+            alt=""
+            width={100}
+            height={100}
+            className="h-full w-full object-cover brightness-90"
+          />
+
+          <HiMiniXCircle
+            className="absolute right-3 top-3 h-7 w-7 cursor-pointer rounded-full text-neutral-100 transition-all duration-300 hover:text-neutral-300"
+            onClick={onRemoveImage}
+          />
+        </div>
+      )}
+
+      <small className="pr-2 text-right font-semibold">
+        {text.length} / {MAX_CHARS}
+      </small>
+    </div>
   );
 };
 export default PostInput;
